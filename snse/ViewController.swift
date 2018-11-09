eb35d46f5daf6b7d5bd0d34a5293ea67e4393c61
Blake Barrett <blake@blakebarrett.net>
2018-11-08 18:15:02 -0800

    Initial Commit

---
+++ snse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snse
+//
+//  Created by Blake Barrett on 11/8/18.
+//  Copyright © 2018 Blake Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
